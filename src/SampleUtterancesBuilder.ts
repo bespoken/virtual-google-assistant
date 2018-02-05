7ef7e7d477eb940ac59bc18b38efa6fdf7f0819c
Juan Perata <juan@bespoken.tools>
2018-02-05 10:54:16 -0500

    Utterance builder

--- src/SampleUtterancesBuilder.ts
+++ src/SampleUtterancesBuilder.ts
@@ -1,40 +1,70 @@
 import * as fs from "fs";
+import * as path from "path";
 import {SampleUtterances} from "virtual-core";
 
-export class SampleUtterancesBuilder {
-    public static fromFile(file: string): SampleUtterances {
-        const data = fs.readFileSync(file);
-        const utterances = new SampleUtterances();
-        SampleUtterancesBuilder.parseFlatFile(utterances, data.toString());
-        return utterances;
-    }
+const INTENT_FOLDER = "intents";
+const AGENT_JSON = "agent.json";
 
-    public static fromJSON(sampleUtterancesJSON: any) {
-        const sampleUtterances = new SampleUtterances();
-        for (const intent of Object.keys(sampleUtterancesJSON)) {
-            for (const sample of sampleUtterancesJSON[intent]) {
-                sampleUtterances.addSample(intent, sample);
-            }
+export class SampleUtterancesBuilder {
+    public static fromFolder(folder: string): SampleUtterances {
+        const intentFolder = path.join(folder, INTENT_FOLDER);
+        if (fs.exists(path.join(folder, AGENT_JSON))) {
+            throw new Error("Missing agent.json, please verify your providing the correct folder");
         }
+
+        if (fs.exists(intentFolder)) {
+            throw new Error("Missing the intents folder, please verify your providing the correct folder");
+        }
+
+        const fileList = fs.readdirSync(intentFolder);
+        const intentFiles = fileList.filter((fileName) => {
+            return !fileName.includes("usersays") && fileName.includes(".");
+        });
+        const utterancesFiles = fileList.filter((fileName) => {
+            return fileName.includes("usersays") && fileName.includes(".");
+        });
+
+        const sampleUtterances = new SampleUtterances();
+        const jsonUtterancesList = utterancesFiles.forEach(( fileName) => {
+            const intentName = fileName.split("_usersays_")[0];
+            const utterances = SampleUtterancesBuilder.extractUtterancesFromFile(folder, fileName);
+            utterances.forEach((utterance) => {
+                sampleUtterances.addSample(fileName, utterance);
+            });
+        });
+
         return sampleUtterances;
     }
 
-    private static parseFlatFile(utterances: SampleUtterances, fileData: string): void {
-        const lines = fileData.split("\n");
-        for (const line of lines) {
-            if (line.trim().length === 0) {
-                // We skip blank lines - which is what Alexa does
-                continue;
-            }
-
-            const index = line.indexOf(" ");
-            if (index === -1) {
-                throw Error("Invalid sample utterance: " + line);
-            }
-
-            const intent = line.substr(0, index);
-            const sample = line.substr(index).trim();
-            utterances.addSample(intent, sample);
+    private static extractUtterancesFromFile(folder: string, fileName: string): string []{
+        const fileData = fs.readFileSync(path.join(folder, INTENT_FOLDER, fileName));
+        const jsonData: IDialogFlowUtterance[] = JSON.parse(fileData.toString());
+        if (!jsonData || !jsonData.length) {
+            return [];
         }
+
+        const utteranceList = jsonData.map((userSpeechDescription: IDialogFlowUtterance) => {
+            return userSpeechDescription.data.reduce((utterance: string, speech: ISpeechDescription) => {
+                if (speech.userDefined) {
+                    return `${utterance}{${speech.alias}}`;
+                } else {
+                    return `${utterance}${speech.text}`;
+                }
+            }, "");
+        });
+
     }
+}
+
+
+interface IDialogFlowUtterance {
+    id: string,
+    data: ISpeechDescription[],
+}
+
+interface ISpeechDescription {
+    text: string,
+    userDefined: boolean,
+    alias?: string,
+    meta?: string,
 }
